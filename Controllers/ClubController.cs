725ba307bd2897a63f063522fb9112f23187ce8f
Jobirmizo <hishimovanvar@gmail.com>
2024-09-16 04:22:13 +0500

    Dependency Injection

---
+++ Controllers/ClubController.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.SqlTypes;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Interfaces;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class ClubController : Controller
+{
+    private readonly IClubService _clubService;
+    
+    public ClubController(IClubService clubService)
+    {
+        _clubService = clubService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        IEnumerable<Club> clubs = await _clubService.GetAll();
+        return View(clubs);
+    }
+
+    public async Task<IActionResult> Detail(int id)
+    {
+        Club club = await _clubService.GetByIdAsync(id);    
+        return View(club);
+    }
+}
---
+++ Controllers/RaceController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WebApplication1.Data;
+using WebApplication1.Interfaces;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class RaceController : Controller
+{
+    private readonly IRaceService _raceService;
+    public RaceController(IRaceService raceService)
+    {
+        _raceService = raceService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        IEnumerable<Race> races = await _raceService.GetAll();
+        return View(races);
+    }
+    public async Task<IActionResult> Detail(int id)
+    {
+        Race race = await _raceService.GetByIdAsync(id);
+        return View(race);
+    }
+}
